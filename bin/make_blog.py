453a2da05982f527a9191e529087c967bb02c475
Tom Shafer <tom@tshafer.com>
2017-05-30 22:40:11 -0400

    Add simple documentation in make_blog.py

---
+++ bin/make_blog.py
@@ -0,0 +1,22 @@
+"""Generate a static blog directory structure.
+
+Usage:
+    make_blog.py PUBLIC_DIR
+
+This program behaves in simply: Point it at a directory that contains Markdown
+files in some configuration, and the program will:
+
+1. Generate in-place HTML files, attaching a template.
+2. Generate an index page (index.html) with either a long list of post titles
+   or a long list of full posts.
+
+The program makes many assumptions about titling, structure, templating, etc.
+These assumptions could be relaxed via, e.g., a YAML configuration file in a
+later update.
+
+"""
+
+from docopt import docopt
+
+if __name__ == "__main__":
+    cli_args = docopt(__doc__)
