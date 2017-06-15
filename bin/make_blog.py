99d7447648cb0f0926032dca6e9f8fcd4d3b3e4f
Tom Shafer <tom@tshafer.com>
2017-06-14 23:33:03 -0400

    Tweaks to render a full blog

--- bin/make_blog.py
+++ bin/make_blog.py
@@ -19,10 +19,20 @@ later update.
 import codecs
 import os
 import re
-import markdown
+from collections import namedtuple
 from dateutil.parser import parse as parse_date
+import markdown
 from docopt import docopt
 from mako.template import Template
+
+
+conf = namedtuple("conf", ("url_base", "url_site_base", "local_base",
+                           "local_tmpl_base"))
+
+conf.url_base = "/blog"
+conf.url_site_base = "/"
+conf.local_base = None
+conf.local_tmpl_base = os.path.expanduser("~/Sites/Personal/2017/assets/template")
 
 
 def find_files(base_dir, extensions=("md",)):
@@ -36,19 +46,22 @@ def find_files(base_dir, extensions=("md",)):
 
 class Post(object):
     """
-    Simple representation of a blog post. Includes:
+    Simple representation of a blog post.
 
-    source: Raw Markdown
-    html:   Parsed HTML
-    title:  Title from the metadata
-    date:   Datetime object
+    source:      Raw Markdown
+    source_path: Full path to source file
+    html:        Parsed HTML
+    title:       Title from the metadata
+    date:        Datetime object
+    slug:        Path to post relative to the blog root
     """
-    title, date, source, html, source_path = 5*[None]
+    title, date, source, html, source_path, slug = 6*[None]
 
     def __init__(self, file_path, md_parser):
-        """Use 'md_parser' to parse 'file_path' into an object"""
+        """Use 'md_parser' to parse 'file_path' into an object."""
         with codecs.open(file_path, mode="r", encoding="utf-8") as fh:
             self.source_path = file_path
+            self.slug = self.make_slug(file_path)
             self.source = fh.read()
             self.html = md_parser.convert(self.source)
             # Assign meta, requiring title and date
@@ -58,16 +71,28 @@ class Post(object):
 
     @staticmethod
     def unquote(string):
-        """Remove quotes from the beginning/end of a string"""
+        """Remove quotes from the beginning/end of a string."""
         return re.sub(r"(^['\"]|['\"]$)", "", string)
+
+    @staticmethod
+    def make_slug(source_path):
+        """
+        Take the source path and make a post slug using the format
+            '<path rel to blog root>.html'.
+
+        Uses the conf global.
+        """
+        relpath = os.path.relpath(source_path, conf.local_base)
+        return "{}.html".format(os.path.splitext(relpath)[0])
 
 
 if __name__ == "__main__":
     cli_args = docopt(__doc__)
+    conf.local_base = cli_args["PUBLIC_DIR"]
 
     # Find Markdown files
     md_extensions = ("md", "mdown", "text")
-    md_files = tuple(find_files(cli_args["PUBLIC_DIR"], md_extensions))
+    md_files = tuple(find_files(conf.local_base, md_extensions))
     if not md_files:
         quit("No markdown files found.")
 
@@ -100,23 +125,19 @@ if __name__ == "__main__":
     # Load templates
     tmpl_post = Template(input_encoding="utf-8",
                          filename=os.path.join(
-                             os.path.dirname(__file__),
-                             "../public/assets/template",
-                             "post_template.html"))
+                             conf.local_tmpl_base, "blog_post_template.html"))
     tmpl_index = Template(input_encoding="utf-8",
                           filename=os.path.join(
-                              os.path.dirname(__file__),
-                              "../public/assets/template",
-                              "index_template.html"))
+                              conf.local_tmpl_base, "blog_index_template.html"))
 
     # Generate HTML from stored objects
     for post in posts:
         html_path = "{}.html".format(os.path.splitext(post.source_path)[0])
         with codecs.open(html_path, mode="w", encoding="utf-8") as fh2:
-            fh2.write(tmpl_post.render(post=post))
+            fh2.write(tmpl_post.render(post=post, conf=conf))
 
-    # Generate index file
-    with codecs.open(filename=os.path.join(os.path.dirname(__file__), "../public/index.html"),
+    # Generate index file at base of blog root
+    with codecs.open(filename=os.path.join(conf.local_base, "index.html"),
                      mode="w", encoding="utf-8")as fh3:
         posts = list(sorted(posts, key=lambda p: p.date, reverse=True))
-        fh3.write(tmpl_index.render_unicode(posts=posts))
+        fh3.write(tmpl_index.render_unicode(posts=posts, conf=conf))
